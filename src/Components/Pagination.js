72b404fdff12e58f0ed54dc25783f72ccb02335c
Sachin Kumar Behera <sachin.behera@attryb.com>
2024-01-30 02:02:57 +0530

    pagination added

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import "./Pagination.css";
+import './Pagination.css';
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
 
   return (
-    <div className='page'>
-      {pageNumbers.map(number => (
-        <button className="nextPage" key={number} onClick={() => onPageChange(number)}>
+    <div className="page">
+      <button
+        className="pagination-button"
+        onClick={() => onPageChange(Math.max(currentPage - 1, 1))}
+        disabled={currentPage === 1}
+      >
+        Prev
+      </button>
+
+      {pageNumbers.map((number) => (
+        <button
+          className={`pagination-button ${currentPage === number ? 'active' : ''}`}
+          key={number}
+          onClick={() => onPageChange(number)}
+        >
           {number}
         </button>
       ))}
+
+      <button
+        className="pagination-button"
+        onClick={() => onPageChange(Math.min(currentPage + 1, totalPages))}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
     </div>
   );
 };
